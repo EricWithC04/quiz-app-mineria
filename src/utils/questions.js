1c804d299f8284938537daeaf85f9526f672cca5
EricWithC04 <negativegame117@gmail.com>
2024-04-17 20:36:23 -0300

    feat: added first questions

---
+++ src/utils/questions.js
@@ -0,0 +1,109 @@
+const questions = [
+    {
+        question: "¿Qué equipo ganará la champions 2024?",
+        answers: [
+            {
+                text: "Bayern",
+                correct: false
+            },
+            {
+                text: "Real Madrid",
+                correct: false
+            },
+            {
+                text: "PSG",
+                correct: false
+            },
+            {
+                text: "Dortmund",
+                correct: true
+            },
+        ]
+    },
+    {
+        question: "¿Cuál es el río más largo del mundo?",
+        answers: [
+            {
+                text: "Amazonas",
+                correct: true
+            },
+            {
+                text: "Nilo",
+                correct: false
+            },
+            {
+                text: "Misisipi",
+                correct: false
+            },
+            {
+                text: "Yangtsé",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "¿Cuál es el país más grande del mundo por área?",
+        answers: [
+            {
+                text: "Rusia",
+                correct: true
+            },
+            {
+                text: "China",
+                correct: false
+            },
+            {
+                text: "Estados Unidos",
+                correct: false
+            },
+            {
+                text: "Canadá",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "¿Quién escribió 'El Principito'?",
+        answers: [
+            {
+                text: "Antoine de Saint-Exupéry",
+                correct: true
+            },
+            {
+                text: "J.K. Rowling",
+                correct: false
+            },
+            {
+                text: "William Shakespeare",
+                correct: false
+            },
+            {
+                text: "Gabriel García Márquez",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "¿Cuál es el elemento más abundante en la corteza terrestre?",
+        answers: [
+            {
+                text: "Oxígeno",
+                correct: false
+            },
+            {
+                text: "Silicio",
+                correct: true
+            },
+            {
+                text: "Hierro",
+                correct: false
+            },
+            {
+                text: "Aluminio",
+                correct: false
+            },
+        ]
+    }
+]
+
+export default questions
